d0604b5571a9727b1a5907366aeda943ca5c374f
Edward He <edward.he@macaulay.cuny.edu>
2016-03-09 14:40:00 -0500

    Make successful GET request to GitHub API. Need to manipulate data.

--- github_users.rb
+++ github_users.rb
@@ -1,6 +1,19 @@
 require 'net/http'
+require 'json'
 require 'csv'
 
+require 'byebug'
+
+begin
+  url = 'https://api.github.com/users?since=0/'
+  uri = URI(url)
+  resp = Net::HTTP.get(uri)
+  data = JSON.parse(resp)
+  byebug
+
+rescue
+   print "Connection error."
+end
 
 
 CSV.open("top_10_github_NY.csv", "wb") do |csv|
