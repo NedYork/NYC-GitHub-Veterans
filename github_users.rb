97bca273868fd8c46c15af4d681d736f6d07049c
Edward He <edward.he@macaulay.cuny.edu>
2016-03-09 17:08:07 -0500

    Refactor all code into methods.

--- github_users.rb
+++ github_users.rb
@@ -4,53 +4,60 @@ require 'csv'
 
 require 'byebug'
 
-
 # GET Request For All Users in NY- GitHub API
-begin
-  # example query
-  # https://api.github.com/search/users?q=tom+repos:%3E42+followers:%3E1000
-  # https://api.github.com/search/users?q=tom+repos:>42+followers:>1000
-  # https://api.github.com/search/users?q=location%3Anew+york&sort=joined&order=desc
-
-  # GitHub API sorts by location: new york and in order which user joined GitHub
-  url = 'https://api.github.com/search/users?q=location%3Anew+york&sort=joined&order=asc'
-  uri = URI(url)
-  resp = Net::HTTP.get(uri)
-  data = JSON.parse(resp)['items']
-
-  usernames = [];
-  data.each do |user|
-    usernames << user['login']
-  end
-
-rescue
-   print "Connection error."
-end
-
-
-# Parse through each user for more specific data
-# Makes 1 request per user
-result = [];
-usernames.each do |username|
+def usernames_get(location)
+  location_query = location.split(" ").join("+")
   begin
-    url = "https://api.github.com/users/#{username}"
+    # GitHub API sorts by location: new york and in order which user joined GitHub
+    url = "https://api.github.com/search/users?q=location%3A" + location_query + "&sort=joined&order=asc"
     uri = URI(url)
     resp = Net::HTTP.get(uri)
-    user = JSON.parse(resp)
-    result << [user['login'], user['name'], user['location'], user['public_repos']]
-    break if result.length == 10
+    data = JSON.parse(resp)['items']
+
+    usernames = [];
+    data.each do |user|
+      usernames << user['login']
+    end
 
   rescue
      print "Connection error."
   end
+  usernames
+end
+
+# Parse through each user for more specific data
+# Makes 1 request per user
+def grab_user_data(usernames_array)
+  result = [];
+  usernames_array.each do |username|
+    begin
+      url = "https://api.github.com/users/#{username}"
+      uri = URI(url)
+      resp = Net::HTTP.get(uri)
+      user = JSON.parse(resp)
+      result << [user['login'], user['name'], user['location'], user['public_repos']]
+      break if result.length == 10
+
+    rescue
+       print "Connection error."
+    end
+  end
+  result
 end
 
 
 # CSV file creation
-CSV.open("top_10_github_NY.csv", "wb") do |csv|
-  csv << ["login", "name", "location", "repo count"]
+def csv_create(user_data_array)
+  CSV.open("top_10_github_NY.csv", "wb") do |csv|
+    csv << ["login", "name", "location", "repo count"]
 
-  result.each do |user|
-    csv << user
+    user_data_array.each do |user|
+      csv << user
+    end
   end
 end
+
+
+usernames = usernames_get("new york")
+user_data = grab_user_data(usernames)
+csv_create(user_data)
