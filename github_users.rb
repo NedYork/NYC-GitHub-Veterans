134f7f02f22a9f81dee8404f3b83375191f6c826
Edward He <edward.he@macaulay.cuny.edu>
2016-03-09 15:24:44 -0500

    Connect API retrieval data with CSV create. Need to fix query logic.

--- github_users.rb
+++ github_users.rb
@@ -4,19 +4,28 @@ require 'csv'
 
 require 'byebug'
 
+# example query
+# https://api.github.com/search/users?q=tom+repos:%3E42+followers:%3E1000
+# https://api.github.com/search/users?q=location%3Anew+york
+
+
+# GET Request - GitHub API
 begin
-  url = 'https://api.github.com/users?since=0/'
+  url = 'https://api.github.com/search/users?q=location%3Anew+york'
   uri = URI(url)
   resp = Net::HTTP.get(uri)
-  data = JSON.parse(resp)
-  byebug
-
+  data = JSON.parse(resp)['items']
+  top_10_users = data[0...10]
 rescue
    print "Connection error."
 end
 
 
+# CSV file creation
 CSV.open("top_10_github_NY.csv", "wb") do |csv|
   csv << ["login", "name", "location", "repo count"]
-  csv << ["user1", "test name", "NY", 5]
+
+  top_10_users.each do |user|
+    csv << [user['login'], user['id'], user['gravatar_id'], user['url']]
+  end
 end
